6d3948811c443af68377ca440f4e953aed69d281
Erigelavaralakshmi <erigelavaralakshmi365@gmail.com>
2024-01-24 11:54:22 +0530

    adding new file

---
+++ function4.py
@@ -0,0 +1,76 @@
+#Circle
+def main():
+    radius = int(input("Enter a radius of the circle: " ))
+    calculate_area(radius)
+def calculate_area(radius):
+    area = 3.14 * radius ** 2
+    return area
+main()
+
+
+#Rectangle
+# def main():
+#     length = int(input("Enter a length of the rectangle: " ))
+#     width = int(input("Enter a width of the rectangle: "))
+#     calculate_area(length,width)  
+# def calculate_area(length,width):
+#     area = length * width
+#     print(area)
+# main()
+
+# #Triangle
+# def main():
+#     base = int(input("Enter a length of the triangle: " ))
+#     height = int(input("Enter a width of the triangle: "))
+#     calculate_area(base,height)  
+# def calculate_area(base,height):
+#     area = 1/2 * base * height
+#     print(area)
+# main()
+
+# #Trapezoid
+# def main():
+#     height = int(input("Enter a length of the trapezoid: " ))
+#     a = int(input("Enter a lenght of a: "))
+#     b = int(input("Enter a lenght of b: "))
+#     calculate_area(height,a,b)  
+# def calculate_area(height,a,b):
+#     area = 1/2 * height * (a+b)
+#     print(area)
+# main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+    
